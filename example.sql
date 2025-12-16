a9be4b01af482af4e3d0eb440e6091b1038564a5
熊大如如 <xxrBear@gmail.com>
2025-12-16 16:29:11 +0800

    update example

--- example.sql
+++ example.sql
@@ -28,3 +28,31 @@ INSERT INTO orders (region, product, order_date, sales) VALUES
 ('America', 'Phone',  '2025-01-22', 900.00),
 ('America', 'Laptop', '2025-02-12', 2100.00),
 ('America', 'Tablet', '2025-02-18', 400.00);
+
+/*
+  连接语句
+*/
+
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    dept_id INT
+);
+
+INSERT INTO department (id, name) VALUES (101, '销售部');
+INSERT INTO department (id, name) VALUES (102, '研发部');
+INSERT INTO department (id, name) VALUES (103, '人事部');
+INSERT INTO department (id, name) VALUES (104, '市场部');
+INSERT INTO department (id, name) VALUES (105, '财务部');
+
+INSERT INTO employee (id, name, dept_id) VALUES (1, '张三', 101); -- 销售部
+INSERT INTO employee (id, name, dept_id) VALUES (2, '李四', 102); -- 研发部
+INSERT INTO employee (id, name, dept_id) VALUES (3, '王五', 103); -- 人事部
+INSERT INTO employee (id, name, dept_id) VALUES (4, '赵六', 105); -- 财务部
+INSERT INTO employee (id, name, dept_id) VALUES (5, '小明', NULL); -- 无部门
+INSERT INTO employee (id, name, dept_id) VALUES (6, '小红', 999); -- 孤立的部门 ID
